3107829075c117b61c921d36a8e2e8d4c3ff2558
max <theTrxMail@proton.me>
2024-12-08 22:56:00 +0800

    dev 后端api逻辑和添加数据库组件，选定sqlite数据库完成持久化保存

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,18 +1,54 @@
 const middlewares = require('@blocklet/sdk/lib/middlewares');
 const router = require('express').Router();
+const { PrismaClient } = require('@prisma/client');
+
+const prismaClient = new PrismaClient();
 
 router.use('/user', middlewares.session(), (req, res) => res.json(req.user || {}));
 
-router.use('/data', (req, res) => {
+router.use('/data', async (req, res) => {
   const defaultUser = {
     name: 'John Doe',
     email: 'john.doe@example.com',
     phone: '123-456-7890',
   };
-  res.json(defaultUser);
+  const users = await prismaClient.user.findMany();
+  if (users.length === 0) {
+    await prismaClient.user.create({
+      data: {
+        name: 'John Doe',
+        email: 'john.doe@example.com',
+        phone: '123-456-7890',
+      },
+    });
+    res.json(defaultUser);
+  }
+  res.json(users[0]);
 });
 
-router.use('/savaDate', (req, res) => {
+router.use('/savaDate', async (req, res) => {
+  const { name, email, phone } = req.body;
+  const users = await prismaClient.user.findMany();
+  if (users.length === 0) {
+    await prismaClient.user.create({
+      data: {
+        name,
+        email,
+        phone,
+      },
+    });
+  } else {
+    await prismaClient.user.update({
+      where: {
+        id: users[0].id,
+      },
+      data: {
+        name,
+        email,
+        phone,
+      },
+    });
+  }
   res.json({
     message: 'success',
   });
